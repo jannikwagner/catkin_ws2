0f0f2709ac5a525f8ea77c7bd408ae0b2f72dc9e
Jannik Wagner <wagne@kth.se>
2022-10-03 11:14:30 +0200

    init 2

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/w/a/wagne/catkin_ws/src
-# Build directory: /afs/kth.se/home/w/a/wagne/catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
--- build/gtest/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googletest
-# Build directory: /afs/kth.se/home/w/a/wagne/catkin_ws/build/gtest/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
