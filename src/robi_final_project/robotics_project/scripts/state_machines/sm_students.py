19f0281398bbaad1a17d8544fcb151e91722db8b
Jannik Wagner <wagne@kth.se>
2022-10-10 10:16:54 +0200

    pick up woks now

--- src/robi_final_project/robotics_project/scripts/state_machines/sm_students.py
+++ src/robi_final_project/robotics_project/scripts/state_machines/sm_students.py
@@ -100,13 +100,31 @@ class StateMachine(object):
         while not rospy.is_shutdown() and self.state != 4:
             rospy.loginfo("%s: check_states while loop", self.node_name)
             
-            # State 0:
+            # State 0:  Tuck arm 
             if self.state == 0:
+                rospy.loginfo("%s: Tucking the arm...", self.node_name)
+                goal = PlayMotionGoal()
+                goal.motion_name = 'home'
+                goal.skip_planning = True
+                self.play_motion_ac.send_goal(goal)
+                success_tucking = self.play_motion_ac.wait_for_result(rospy.Duration(100.0))
+
+                if success_tucking:
+                    rospy.loginfo("%s: Arm tuck: ", self.play_motion_ac.get_result())
+                    self.state = 1
+                else:
+                    self.play_motion_ac.cancel_goal()
+                    rospy.logerr("%s: play_motion failed to tuck arm, reset simulation", self.node_name)
+                    self.state = 5
+
+                rospy.sleep(1)
+
+            # State 1: Pick up cube
+            if self.state == 1:
 
                 self.aruco_pose_pub.publish(self.cube_pose) # Publish the aruco pose for manip client to receive
 
 
-            # try:
                 rospy.loginfo("%s: Pick up goal", self.node_name)
                 pick_srv = rospy.ServiceProxy(self.pick_srv_nm, SetBool)
                 
@@ -115,22 +133,35 @@ class StateMachine(object):
 
                 
                 if pick_srv_req.success == True:
-                    self.state = 4
+                    self.state = 2
                     rospy.loginfo("%s: Pickup succeeded!", self.node_name)
                 else:
                     rospy.loginfo("%s: Pickup failed!", self.node_name)
                     self.state = 5
 
                 rospy.sleep(3)
-            
-            # except rospy.ServiceException, e:
-            #     print "Service call to move_head server failed: %s"%e
-            #     raise RuntimeError()
-                
 
 
-            # State 0: Move the robot "manually" to door
-            if self.state == -1:
+            # State 2: Rotate the robot "manually"
+            if self.state == 2:
+                move_msg = Twist()
+                move_msg.angular.z = 1
+
+                rate = rospy.Rate(10)
+                converged = False
+                cnt = 0
+                rospy.loginfo("%s: Rotating", self.node_name)
+                while not rospy.is_shutdown() and cnt < 25:
+                    self.cmd_vel_pub.publish(move_msg)
+                    rate.sleep()
+                    cnt = cnt + 1
+
+                self.state = 3
+                rospy.sleep(1)
+
+
+            # State 3: Move the robot "manually" to other table
+            if self.state == 3:
                 move_msg = Twist()
                 move_msg.linear.x = 1
 
@@ -143,27 +174,9 @@ class StateMachine(object):
                     rate.sleep()
                     cnt = cnt + 1
 
-                self.state = 1
+                self.state = 4
                 rospy.sleep(1)
 
-            # State 1:  Tuck arm 
-            if self.state == -1:
-                rospy.loginfo("%s: Tucking the arm...", self.node_name)
-                goal = PlayMotionGoal()
-                goal.motion_name = 'home'
-                goal.skip_planning = True
-                self.play_motion_ac.send_goal(goal)
-                success_tucking = self.play_motion_ac.wait_for_result(rospy.Duration(100.0))
-
-                if success_tucking:
-                    rospy.loginfo("%s: Arm tuck: ", self.play_motion_ac.get_result())
-                    self.state = 2
-                else:
-                    self.play_motion_ac.cancel_goal()
-                    rospy.logerr("%s: play_motion failed to tuck arm, reset simulation", self.node_name)
-                    self.state = 5
-
-                rospy.sleep(1)
 
             # State 2:  Move the robot "manually" to chair
             if self.state == -1:
